3094bcd883231d3335c9c60e769088c8dd31a6fb
Sanket <sanketbhat882002@gmail.com>
2023-04-01 16:35:55 +0530

    api tokens

--- api/endpoints/generate_image.py
+++ api/endpoints/generate_image.py
@@ -1,80 +1,44 @@
-from fastapi import APIRouter
-from io import BytesIO
-from PIL import Image, ImageDraw, ImageFont
-from fastapi.responses import StreamingResponse
 import requests
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # load environment variables from .env file
+
+API_TOKEN = os.environ.get("REPLICATE_API_TOKEN")
+
 router = APIRouter()
 
-@router.post('/generate_image')
-async def generate_image(prompt: str):
-    # Generate the image bytes using Stable Diffusion or Dalle
-    image_bytes = generate_image_using_stable_diffusion(prompt)
-    
-    # Return the image bytes as a streaming response
-    return StreamingResponse(BytesIO(image_bytes), media_type="image/png")
+class GenerateImageRequest(BaseModel):
+    prompt: str
 
-def generate_image_using_stable_diffusion(prompt: str) -> bytes:
-    # TODO: Implement function to generate image using Stable Diffusion or Dalle
-    # Here's an example of how you might use the Unsplash API to get a random image:
-    response = requests.get("https://api.unsplash.com/photos/random", headers={
-        "Accept-Version": "v1",
-        "Authorization": "Client-ID gG_ClSiPX9v9pEPonerXlUu28wrClQjOKYujnPkDoWo"
-    })
-    image_url = response.json()["urls"]["regular"]
-    
-    # Open the image from the URL and add the quote text
-    response = requests.get(image_url)
-    image = Image.open(BytesIO(response.content)).convert("RGBA")
-    draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype("arial.ttf", size=24)
-    draw.text((50, 50), prompt, font=font, fill=(255, 255, 255, 255))
-    
-    # Save the image to a byte stream and return it
-    output = BytesIO()
-    image.save(output, format="PNG")
-    return output.getvalue()
+class GenerateImageResponse(BaseModel):
+    image_url: str
 
+@router.post("/generate-image", response_model=GenerateImageResponse)
+async def generate_image(request: GenerateImageRequest):
+    # Generate image using Stable Diffusion API
+    try:
+        response = requests.post(
+            "https://api.replicate.ai/v1/model/stability-ai/stable-diffusion/generate",
+            json={
+                "prompt": request.prompt,
+                "num_images": 1,
+                "size": 512
+            },
+            headers = {
+                "Authorization": f"Bearer {API_TOKEN}",
+                "Content-Type": "application/json"
+            }
+        )
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail="Failed to generate image") from e
 
-
-# from fastapi import APIRouter
-# from PIL import Image, ImageDraw, ImageFont
-# from dalle_mini import load_model, generate_images
-# import requests
-# from io import BytesIO
-
-# router = APIRouter()
-
-# # Load the DALL-E model
-# model = load_model("dalle-mini")
-
-# @router.post("/generate_image")
-# async def generate_image(prompt: str):
-#     # Generate the image using the DALL-E model
-#     image = generate_images(
-#         prompt=prompt,
-#         model=model,
-#         batch_size=1,
-#         top_k=1,
-#         temperature=1.0
-#     )[0]
-
-#     # Convert the image from a NumPy array to a PIL Image
-#     image = Image.fromarray(image)
-
-#     # Resize the image to a fixed size
-#     image = image.resize((800, 600))
-
-#     # Add the quote to the image using a specified font and color
-#     draw = ImageDraw.Draw(image)
-#     font = ImageFont.truetype("arial.ttf", 50)
-#     text_color = (255, 255, 255)
-#     text_position = (50, 50)
-#     draw.text(text_position, prompt, font=font, fill=text_color)
-
-#     # Convert the PIL Image to a bytes object for streaming to the client
-#     image_bytes = BytesIO()
-#     image.save(image_bytes, format="JPEG")
-#     image_bytes = image_bytes.getvalue()
-
-#     # Return the image as a streaming response
-#     return StreamingResponse(io.BytesIO(image_bytes), media_type="image/jpeg")
+    try:
+        # Extract image URL from the response
+        image_url = response.json()["result"][0]["path"]
+        return GenerateImageResponse(image_url=image_url)
+    except (KeyError, IndexError):
+        raise HTTPException(status_code=500, detail="Failed to extract image URL from response")
--- api/endpoints/get_prompt.py
+++ api/endpoints/get_prompt.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI, HTTPException
 import openai
 from fastapi import APIRouter
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
 router = APIRouter()
 
 def generate_prompt_from_chatgpt(quote: str) -> str:
     # Initialize OpenAI API
-    openai.api_key = "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"
+    openai.api_key = openai_api_key
 
     # Generate prompt using OpenAI's GPT-3 model
     prompt = "Generate a text description for the following quote: " + quote
