d029f171dad74b3df689a77e01f08507e8134427
Sanket <sanketbhat882002@gmail.com>
2023-04-04 09:53:57 +0530

    working main for stable diffusion

--- api/models/prompt.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class Prompt(BaseModel):
-    quote: str
-    image_size: str = "1024x1024"
-    font_family: str = "Arial"
-    font_color: str = "#000000"
-    background_color: str = "#FFFFFF"
